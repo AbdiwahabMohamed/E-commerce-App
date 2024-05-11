3a63f3540afa4294bfcd3d9e8172f92595f5a9ec
Abdiwahab Mohamed <Fahadfar69@gmail.com>
2024-05-11 13:49:24 +0300

    Product Services

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,29 @@
 /* eslint-disable prettier/prettier */
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
+  @Get()
+  getProducts() {
+    return this.productsService.getProducts();
+  }
+
+  @Get(':id')
+  getProductById(id: number) {
+    return this.productsService.getProductById(id);
+  }
+  @Post()
+  createProduct(product: any) {
+    return this.productsService.createProduct(product);
+  }
+  @Put(':id')
+  updateProduct(id: number) {
+    return this.productsService.updateProduct(id);
+  }
+  @Delete(':id')
+  deleteProduct(id: number) {
+    return this.productsService.deleteProduct(id);
+  }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,5 +1,53 @@
 /* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(private databaseService: DatabaseService) {}
+  async getProducts() {
+    return await this.databaseService.product.findMany();
+  }
+
+  async getProductById(id: number) {
+    const findProduct = await this.databaseService.product.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    if (!findProduct) {
+      throw new NotFoundException('Product not found');
+    }
+  }
+  async createProduct(product: any) {
+    return await this.databaseService.product.create({
+      data: product,
+    });
+  }
+  async updateProduct(id: number) {
+    const findProduct = await this.databaseService.product.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!findProduct) {
+      throw new NotFoundException('Product not found');
+    }
+  }
+  async deleteProduct(id: number) {
+    const findProduct = await this.databaseService.product.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    if (!findProduct) {
+      throw new NotFoundException('Product not found');
+    }
+    return await this.databaseService.product.delete({
+      where: {
+        id: id,
+      },
+    });
+  }
+}
